224c7926e4acba9233bd93a7a3bc366e8f0f7f19
mre-dev <mre.dev01@gmail.com>
2022-04-07 01:07:51 +0430

    Update Header

--- src/layouts/header/header.layout.js
+++ src/layouts/header/header.layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Styles from './header.module.css';
+import { BiUserCircle } from 'assets/images/icons';
 
 export const Header = (props) => {
     return (
-        <header>
-            <p>Header</p>
+        <header className={Styles.header}>
+            <h3>Welcome To Dashboard</h3>
+            <BiUserCircle />
         </header>
     );
 }
