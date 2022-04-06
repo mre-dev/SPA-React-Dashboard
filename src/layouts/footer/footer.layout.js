b090aa3030341b12142d11b5f75584aa5b8dac04
mre-dev <mre.dev01@gmail.com>
2022-04-07 01:07:33 +0430

    Update Footer

--- src/layouts/footer/footer.layout.js
+++ src/layouts/footer/footer.layout.js
@@ -1,10 +1,11 @@
+import { Navigation } from 'components';
 import React from 'react';
 import Styles from './footer.module.css';
 
 export const Footer = (props) => {
     return (
-        <footer>
-            <p>Footer</p>
+        <footer className={Styles.footer}>
+            <h2><Navigation text="Mohammad Reza Ebrahimi" link='https://mre01.ir' external/></h2>
         </footer>
     );
 }
