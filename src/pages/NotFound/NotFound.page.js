9c8c11c524d4b8579623273995c86e581a6eeca1
mre-dev <mre.dev01@gmail.com>
2022-04-07 01:52:36 +0430

    Create Not Fount Page

--- src/pages/NotFound/NotFound.page.js
+++ src/pages/NotFound/NotFound.page.js
@@ -1,9 +1,13 @@
+import { Navigation } from 'components';
+import { PATHS } from 'configs/routes.config';
 import React from 'react';
+import Styles from './notFound.module.css';
 
 export const NotFoundPage = (props) => {
     return (
-        <div>
-            Not Found Page
+        <div className={Styles.notFound}>
+            <p>Page Not Found</p>
+            <Navigation link={PATHS.HOME} text="Go to Home" internal/>
         </div>
     );
 };
